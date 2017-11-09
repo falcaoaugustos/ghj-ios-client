149306191128c1b6012b8e1294b29b4e5d7557fc
Augusto Falcao <falcaoaugustos@gmail.com>
2017-11-09 15:50:40 -0300

    feat (model): create job model

---
+++ ghj/Models/Job.swift
@@ -0,0 +1,21 @@
+//
+//  Job.swift
+//  ghj
+//
+//  Created by Augusto Falcão on 11/9/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import Foundation
+
+struct Job {
+    let id: String
+    let createAt: String
+    let title: String
+    let location: String
+    let type: String
+    let company: String
+    let companyUrl: String
+    let companyLogo: String
+    let url: String
+}
