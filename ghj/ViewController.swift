974bda1e4c3d3fc6f93ba8f8da6bd6e8f0065cc0
Augusto Falcao <falcaoaugustos@gmail.com>
2017-11-10 00:13:10 -0300

    feat (feed interface): implement feed table view controller

--- ghj/ViewController.swift
+++ ghj/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    var jobs = [Job]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(UINib.init(nibName: "JobTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "JobCell")
+        jobs.append(Job(
+            id: "123",
+            createAt: "Today",
+            title: "Web Developer",
+            location: "Fortaleza",
+            type: "Fulltime",
+            company: "NYC",
+            companyUrl: "www.nyc.com",
+            companyLogo: "www.nyc.com/logo",
+            url: "www.nyc.com"
+        ))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Data Source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jobs.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "JobCell", for: indexPath) as! JobTableViewCell
+        cell.job = jobs[indexPath.row]
+        cell.loadData()
+
+        return cell
+    }
+
+    // MARK: Delegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("BOM")
+    }
 
 }
 
