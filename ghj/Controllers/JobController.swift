4a2d5797fe25e12d3bfd63c2cf647a8a5c607ebc
Augusto Falcao <falcaoaugustos@gmail.com>
2017-11-14 02:02:32 -0300

    feat (interface): implement job information view

--- ghj/Controllers/JobController.swift
+++ ghj/Controllers/JobController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2017 Augusto Falcão. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class JobController: UIViewController {
+
+    @IBOutlet var propertiesTextView: UITextView!
+
+    var job: Job?
+
+    override func viewDidLoad() {
+        setupProperties()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    private func setupProperties() {
+        guard let job = job else { return }
+
+        propertiesTextView.text = """
+        Title: \(job.title)
+        Location: \(job.location)
+        Type: \(job.type)
+        Link: \(job.url)
+        Company: \(job.company)
+        Company URL: \(job.companyUrl)
+        """
+    }
+
+    @IBAction func dismissController(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
